c725216f0acfecdd359f641e107b318933c36b9e
Diman Andreev <dslads@gmail.com>
2019-12-24 15:42:25 +0300

    added Option to case class types added custom method printThis() to "pretty print" into case class

--- HW4_Lesson5/json_reader_andreev/src/main/scala/com/github/adm8/de/hw4/JsonReader.scala
+++ HW4_Lesson5/json_reader_andreev/src/main/scala/com/github/adm8/de/hw4/JsonReader.scala
@@ -4,7 +4,20 @@ import org.apache.spark.sql.SparkSession
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
-case class WinePosition(id: Int, points: Int, price: Double, country: String, variety: String, title: String, winery: String )
+case class WinePosition(id:Option[Int], points:Option[Int], price:Option[Double], country:Option[String], variety:Option[String], title:Option[String], winery:Option[String] ){
+  def printThis(): Unit ={
+    var str = "WinePosition("
+    str += this.id.getOrElse(None).toString
+    str += "," + this.points.getOrElse(None).toString
+    str += "," + this.price.getOrElse(None).toString
+    str += "," + this.country.getOrElse(None).toString
+    str += "," + this.variety.getOrElse(None).toString
+    str += "," + this.title.getOrElse(None).toString
+    str += "," + this.winery.getOrElse(None).toString
+    str += ")"
+    println(str)
+  }
+}
 
 object JsonReader extends App {
 
@@ -22,32 +35,28 @@ object JsonReader extends App {
     // получаем данные
     val jsonLine = parse(line)
 
-    val id_str = pretty(jsonLine \\ "id")
-    val price_str = pretty(jsonLine \\ "price")
-    val points_str = pretty(jsonLine \\ "points")
-    var country = pretty(jsonLine \\ "country")
-    var variety = pretty(jsonLine \\ "variety")
-    var title = pretty(jsonLine \\ "title")
-    var winery = pretty(jsonLine \\ "winery")
+    val id_str:Option[String] = Some(pretty(jsonLine \\ "id"))
+    val price_str:Option[String] = Some(pretty(jsonLine \\ "price"))
+    val points_str:Option[String] = Some(pretty(jsonLine \\ "points"))
+    var country:Option[String] = Some(pretty(jsonLine \\ "country"))
+    var variety:Option[String] = Some(pretty(jsonLine \\ "variety"))
+    var title:Option[String] = Some(pretty(jsonLine \\ "title"))
+    var winery:Option[String] = Some(pretty(jsonLine \\ "winery"))
 
-    val id = if (id_str == "{ }") 0 else id_str.toInt
-    val points = if (points_str == "{ }") 0 else points_str.toInt
-    val price = if (price_str == "{ }") 0 else price_str.toDouble
-    country = if (country == "{ }") "" else  country
-    variety = if (variety == "{ }") "" else  variety
-    title = if (title == "{ }") "" else  title
-    winery = if (winery == "{ }") "" else  winery
+    val id:Option[Int] = if (id_str.get == "{ }") None else Some(id_str.get.toInt)
+    val points:Option[Int] = if (points_str.get == "{ }") None else Some(points_str.get.toInt)
+    val price:Option[Double] = if (price_str.get == "{ }") None else Some(price_str.get.toDouble)
+    country = if (country.get == "{ }") None else  Some(country.get)
+    variety = if (variety.get == "{ }") None else  Some(variety.get)
+    title = if (title.get == "{ }") None else  Some(title.get)
+    winery = if (winery.get == "{ }") None else  Some(winery.get)
+
 
     // прокидываем в кейс-класс
-    val wine = WinePosition(id, points, price, country, variety, title, winery)
-    // печатаем
-    println(wine)
-
+    WinePosition(id, points, price, country, variety, title, winery).printThis()
 
 
   })
-
-
 
 
 }
