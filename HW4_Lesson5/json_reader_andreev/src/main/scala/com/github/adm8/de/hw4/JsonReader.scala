b16956c13d0b3b760b79c3d222abf1541e81c328
Diman Andreev <dslads@gmail.com>
2019-12-24 11:47:01 +0300

    fixed problem with { } case class types was changed to appropriate

--- HW4_Lesson5/json_reader_andreev/src/main/scala/com/github/adm8/de/hw4/JsonReader.scala
+++ HW4_Lesson5/json_reader_andreev/src/main/scala/com/github/adm8/de/hw4/JsonReader.scala
@@ -4,7 +4,7 @@ import org.apache.spark.sql.SparkSession
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
-case class WinePosition(id: String, price: String, country: String, variety: String, title: String, winery: String )
+case class WinePosition(id: Int, points: Int, price: Double, country: String, variety: String, title: String, winery: String )
 
 object JsonReader extends App {
 
@@ -12,6 +12,8 @@ object JsonReader extends App {
   val spark = SparkSession.builder().master("local").getOrCreate()
   val sc = spark.sparkContext
   val filePath = args(0)
+  //val filePath = "C:\\temp\\winemag-data-130k-v2.json"
+
   val rddFile = sc.textFile(filePath)
 
   // пробегаемся по файлу
@@ -20,18 +22,28 @@ object JsonReader extends App {
     // получаем данные
     val jsonLine = parse(line)
 
-    val id = pretty(jsonLine \\ "id")
-    val price = pretty(jsonLine \\ "price")
-    val country = pretty(jsonLine \\ "country")
-    val variety = pretty(jsonLine \\ "variety")
-    val title = pretty(jsonLine \\ "title")
-    val winery = pretty(jsonLine \\ "winery")
+    val id_str = pretty(jsonLine \\ "id")
+    val price_str = pretty(jsonLine \\ "price")
+    val points_str = pretty(jsonLine \\ "points")
+    var country = pretty(jsonLine \\ "country")
+    var variety = pretty(jsonLine \\ "variety")
+    var title = pretty(jsonLine \\ "title")
+    var winery = pretty(jsonLine \\ "winery")
+
+    val id = if (id_str == "{ }") 0 else id_str.toInt
+    val points = if (points_str == "{ }") 0 else points_str.toInt
+    val price = if (price_str == "{ }") 0 else price_str.toDouble
+    country = if (country == "{ }") "" else  country
+    variety = if (variety == "{ }") "" else  variety
+    title = if (title == "{ }") "" else  title
+    winery = if (winery == "{ }") "" else  winery
 
     // прокидываем в кейс-класс
-    val wine = WinePosition(id, price, country, variety, title, winery)
-
+    val wine = WinePosition(id, points, price, country, variety, title, winery)
     // печатаем
     println(wine)
+
+
 
   })
 
